752ccf5b24498f65ad04f0e082ada30430adffac
Anusky <anusky.pitrusky@gmail.com>
2021-09-19 19:52:23 +0200

    main - wip - covered 404 page

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,11 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      asPath: () => "",
+    };
+  },
+}));
